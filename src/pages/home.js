8cc195e678075e975fd9b8d9a19455f5e1d1cfc8
devzonedz <teachingscripts@gmail.com>
2020-12-09 11:12:11 +0100

    home

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,140 +12,69 @@ import { BsArrowUpLeft } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 // import fx from 'money';
 import { connect } from 'react-redux';
-import { getBooks } from '../redux/actions/booksActions';
+import { getHome } from '../redux/actions/homeActions';
 
 import CatBooks from '../components/books/catBooks';
 
-function Home(props) {
+function Home({ getHome }) {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [data, setData] = React.useState(null);
+  const [loaded, setLoaded] = React.useState(false);
+  const imageLoaded = () => {
+    setLoaded(true);
+  };
+  React.useEffect(() => {
+    async function getData() {
+      const res = await getHome();
+      console.log(res);
+      if (res) {
+        setData(res.data);
+      }
+    }
+    getData();
+  }, []);
 
   const color = { light: 'black', dark: 'white' };
   return (
     <Box pr="10%" pl="10%" mt="100px" mb="100px">
       <Image src="https://48428-125698-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/06/black_lives_matter_in_charlotte1800x675-1600x600.jpg"></Image>
       <SimpleGrid mt="8" spacing={4} columns={[1, 2, 2, 3]}>
-        <Box p="8">
-          <Heading>
-            هل يمكن أن تكون حالات الطوارئ الوطنية مهد الديمقراطية؟
-          </Heading>
-          <Text fontSize="xl">
-            مع انهيار الحواجز أمام الديمقراطية الرقمية ، فإن السبب الوحيد لعدم
-            المشاركة الجماهيرية هو الحجة القائلة بأن الناس العاديين غير قادرين
-            على اتخاذ القرار السياسي.
-          </Text>
-          <Image
-            mt="4"
-            src="https://48428-125698-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/06/dan_blog_hero_16-9-640x360.jpg"
-          ></Image>
-        </Box>
-        <Box p="8">
-          <Heading>الحرب على المعوقين الحرب على المعوقين</Heading>
-          <Text fontSize="xl">الرأسمالية والرفاهية وصنع كارثة بشرية</Text>
-          <Text fontSize="xl" bg="gray.30">
-            إلين كليفورد
-          </Text>
-          <Text fontSize="xl">
-            ينفخ الغطاء على ما وصفته الأمم المتحدة بـ "كارثة إنسانية" - حرب
-            الحكومة على المعاقين
-          </Text>
-          <Image
-            mt="4"
-            src="https://48428-125698-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/12/P-1592532741-The-War-on-Disabled-People-219x350.jpg"
-          ></Image>
-        </Box>
-        <Box p="8">
-          <Heading>
-            هل يمكن أن تكون حالات الطوارئ الوطنية مهد الديمقراطية؟
-          </Heading>
-          <Text fontSize="xl">
-            مع انهيار الحواجز أمام الديمقراطية الرقمية ، فإن السبب الوحيد لعدم
-            المشاركة الجماهيرية هو الحجة القائلة بأن الناس العاديين غير قادرين
-            على اتخاذ القرار السياسي.
-          </Text>
-          <Image
-            mt="4"
-            src="https://48428-125698-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/06/dan_blog_hero_16-9-640x360.jpg"
-          ></Image>
-        </Box>
-        <Box p="8">
-          <Heading>الحرب على المعوقين الحرب على المعوقين</Heading>
-          <Text fontSize="xl">الرأسمالية والرفاهية وصنع كارثة بشرية</Text>
-          <Text fontSize="xl" bg="gray.30">
-            إلين كليفورد
-          </Text>
-          <Text fontSize="xl">
-            ينفخ الغطاء على ما وصفته الأمم المتحدة بـ "كارثة إنسانية" - حرب
-            الحكومة على المعاقين
-          </Text>
-          <Image
-            mt="4"
-            src="https://48428-125698-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/12/P-1592532741-The-War-on-Disabled-People-219x350.jpg"
-          ></Image>
-        </Box>
-        <Box p="8">
-          <Heading>
-            هل يمكن أن تكون حالات الطوارئ الوطنية مهد الديمقراطية؟
-          </Heading>
-          <Text fontSize="xl">
-            مع انهيار الحواجز أمام الديمقراطية الرقمية ، فإن السبب الوحيد لعدم
-            المشاركة الجماهيرية هو الحجة القائلة بأن الناس العاديين غير قادرين
-            على اتخاذ القرار السياسي.
-          </Text>
-          <Image
-            mt="4"
-            src="https://48428-125698-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/06/dan_blog_hero_16-9-640x360.jpg"
-          ></Image>
-        </Box>
-        <Box p="8">
-          <Heading>الحرب على المعوقين الحرب على المعوقين</Heading>
-          <Text fontSize="xl">الرأسمالية والرفاهية وصنع كارثة بشرية</Text>
-          <Text fontSize="xl" bg="gray.30">
-            إلين كليفورد
-          </Text>
-          <Text fontSize="xl">
-            ينفخ الغطاء على ما وصفته الأمم المتحدة بـ "كارثة إنسانية" - حرب
-            الحكومة على المعاقين
-          </Text>
-          <Image
-            mt="4"
-            src="https://48428-125698-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/12/P-1592532741-The-War-on-Disabled-People-219x350.jpg"
-          ></Image>
-        </Box>
-        <Box p="8">
-          <Heading>
-            هل يمكن أن تكون حالات الطوارئ الوطنية مهد الديمقراطية؟
-          </Heading>
-          <Text fontSize="xl">
-            مع انهيار الحواجز أمام الديمقراطية الرقمية ، فإن السبب الوحيد لعدم
-            المشاركة الجماهيرية هو الحجة القائلة بأن الناس العاديين غير قادرين
-            على اتخاذ القرار السياسي.
-          </Text>
-          <Image
-            mt="4"
-            src="https://48428-125698-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/06/dan_blog_hero_16-9-640x360.jpg"
-          ></Image>
-        </Box>
-        <Box p="8">
-          <Heading>الحرب على المعوقين الحرب على المعوقين</Heading>
-          <Text fontSize="xl">الرأسمالية والرفاهية وصنع كارثة بشرية</Text>
-          <Text fontSize="xl" bg="gray.30">
-            إلين كليفورد
-          </Text>
-          <Text fontSize="xl">
-            ينفخ الغطاء على ما وصفته الأمم المتحدة بـ "كارثة إنسانية" - حرب
-            الحكومة على المعاقين
-          </Text>
-          <Image
-            mt="4"
-            src="https://48428-125698-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/12/P-1592532741-The-War-on-Disabled-People-219x350.jpg"
-          ></Image>
-        </Box>
+        {data &&
+          data.articles &&
+          data.articles.map(article => (
+            <Box>
+              <Heading> {article.title} </Heading>
+              <Box
+                fontSize="xl"
+                className="content event-body"
+                dangerouslySetInnerHTML={{ __html: article.body }}
+              ></Box>
+              <Image
+                src={`${process.env.REACT_APP_STORAGE}/${article.image}`}
+              ></Image>
+            </Box>
+          ))}
+      </SimpleGrid>
+      <SimpleGrid mt="8" spacing={4} columns={[1, 2, 2, 3]}>
+        {data &&
+          data.books &&
+          data.books.map(book => (
+            <Box>
+              <Heading> {book.title} </Heading>
+              <Text fontSize="xl"> {book.overview} </Text>
+              <Image
+                mt="2"
+                w="300px"
+                src={`${process.env.REACT_APP_STORAGE}/${book.cover}`}
+              ></Image>
+            </Box>
+          ))}
       </SimpleGrid>
     </Box>
   );
 }
 const mapDispatchToProps = dispatch => {
-  return { getBooks: () => dispatch(getBooks()) };
+  return { getHome: () => dispatch(getHome()) };
 };
 
 export default connect(null, mapDispatchToProps)(Home);
