46d493328e755f74ca1d5c50427343e5ed35bf3e
devzonedz <devzonegithub@gmail.com>
2021-05-24 12:22:23 +0100

    author book

--- src/pages/ourWriters/Author.js
+++ src/pages/ourWriters/Author.js
@@ -97,8 +97,8 @@ function Author({ getAuthor }) {
                     <Box mb="4" cursor="pointer">
                       <Image
                         loading="lazy"
-                        w="225px"
-                        h="280px"
+                        // w="225px"
+                        // h="280px"
                         m="0 auto"
                         shadow="lg"
                         src={`${process.env.REACT_APP_STORAGE}/${book.cover}`}
