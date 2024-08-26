d704fdf6d8a1414b0c055661128099e38cac5824
mohamed <ma.belhadj@esi-sba.dz>
2024-08-26 12:49:22 +0100

    center the filter

--- src/components/books/BooksFilter.js
+++ src/components/books/BooksFilter.js
@@ -28,7 +28,6 @@ import {
   Spinner,
   useDisclosure,
 } from '@chakra-ui/core';
-// import { ChevronDownIcon } from '@chakra-ui/icons';
 import { NavLink, Link } from 'react-router-dom';
 import { FaChevronDown, FaTimes } from 'react-icons/fa';
 import { GiHamburgerMenu } from 'react-icons/gi';
@@ -61,7 +60,7 @@ function Navbar({ getSeries, getCat }) {
         console.error('Expected an array for categories, but got:', categories);
         setCat([]);
       }
-      
+
       const series = await getSeries();
       if (Array.isArray(series)) {
         setData(series);
@@ -72,7 +71,6 @@ function Navbar({ getSeries, getCat }) {
     }
     getData();
   }, [getCat, getSeries]);
-  
 
   const handleToggle = () => setShow(!show);
 
@@ -93,6 +91,7 @@ function Navbar({ getSeries, getCat }) {
       overflowX={{ base: 'auto', sm: 'auto' }}
       wrap={['nowrap', 'nowrap', 'wrap', 'wrap']}
       direction={['row', 'row', 'row', 'row']}
+      justifyContent="center" // Centering the items
     >
       {!isLargerThan790 && (
         <IconButton
@@ -183,11 +182,8 @@ function Navbar({ getSeries, getCat }) {
           <Menu onOpen={onOpened} onClose={onClosed}>
             <MenuButton
               display="block"
-              // px={4}
-              // py={2}
               whiteSpace="nowrap"
               as={Text}
-              //   mt={{ base: 4, md: 0 }}
               p="4"
               fontSize={['lg', '2xl']}
               fontWeight="bold"
@@ -204,8 +200,6 @@ function Navbar({ getSeries, getCat }) {
                 bg: hoverBg[colorMode],
                 color: hoverClr[colorMode],
               }}
-              // _hover={{ bg: 'gray.100' }}
-              // _focus={{ outline: 0, boxShadow: 'outline' }}
               fontFamily="diodrum-med !important"
               cursor="pointer"
             >
@@ -227,7 +221,7 @@ function Navbar({ getSeries, getCat }) {
               bg={filter[colorMode]}
             >
               {!data && (
-                <Box w="100" mt="50" mb="50" textAlign="center">
+                <Box w="100%" mt="50" mb="50" textAlign="center">
                   <Spinner size="xl" />
                 </Box>
               )}
@@ -241,9 +235,8 @@ function Navbar({ getSeries, getCat }) {
                   cat.map(category => {
                     if (isSmallerThan420) {
                       return (
-                        <Center>
+                        <Center key={category.id}>
                           <Link
-                            key={category.id}
                             style={{ margin: '50px !important' }}
                             onClick={handleToggle}
                             to={`/books_by_category?category=${category.key}`}
@@ -301,11 +294,8 @@ function Navbar({ getSeries, getCat }) {
           <Menu onOpen={onOpened} onClose={onClosed}>
             <MenuButton
               display="block"
-              // px={4}
-              // py={2}
               whiteSpace="nowrap"
               as={Text}
-              //   mt={{ base: 4, md: 0 }}
               p="4"
               fontSize={['lg', '2xl']}
               fontWeight="bold"
@@ -322,12 +312,10 @@ function Navbar({ getSeries, getCat }) {
                 bg: hoverBg[colorMode],
                 color: hoverClr[colorMode],
               }}
-              // _hover={{ bg: 'gray.100' }}
-              // _focus={{ outline: 0, boxShadow: 'outline' }}
               fontFamily="diodrum-med !important"
               cursor="pointer"
             >
-              سلسلات
+              حسب السلسلات
               <FaChevronDown
                 style={{
                   display: 'inline',
@@ -338,329 +326,92 @@ function Navbar({ getSeries, getCat }) {
               ></FaChevronDown>{' '}
             </MenuButton>
             <MenuList
-  h="60vh"
-  w="100vw"
-  color="white"
-  overflowY={{ base: 'scroll', sm: 'scroll' }}
-  bg={filter[colorMode]}
->
-  {data.length === 0 ? (
-    <Box w="100%" mt="50" mb="50" textAlign="center">
-      <Spinner size="xl" />
-    </Box>
-  ) : (
-    <SimpleGrid
-      pl={['5%', '5%', '15%', '15%']}
-      pr={['5%', '5%', '15%', '15%']}
-      py={['5%', '0%', '0%', '0%']}
-      columns={[1, 2, 3, 3, 4]}
-    >
-      {data.map(serie => (
-        <Link
-          style={{ margin: '50px !important' }}
-          onClick={handleToggle}
-          to={`/books_by_series?serie=${serie.name}`}
-          key={serie.id}
-        >
-          <Box m={['0.9', '4']}>
-            <MenuItem
-              _focus={{ bg: 'white', color: 'black' }}
-              _hover={{ bg: 'white', color: 'black' }}
-              fontSize="xl"
+              h="60vh"
+              w="100vw"
+              color="white"
+              overflowY={{ base: 'scroll', sm: 'scroll' }}
+              bg={filter[colorMode]}
             >
-              <Box textAlign="right" display="flex">
-                <Heading
-                  dir="rtl"
-                  fontFamily="diodrum-med !important"
-                  fontSize={['lg', 'lg', 'xl', 'xl']}
-                >
-                  {serie.name}
-                </Heading>
-              </Box>
-            </MenuItem>
-          </Box>
-        </Link>
-      ))}
-    </SimpleGrid>
-  )}
-</MenuList>
-
-          </Menu>
-          {/* <Link>
-        <Text
-          whiteSpace="nowrap"
-          fontWeight="bold"
-          ml="8"
-          fontSize={['lg', '2xl']}
-        >
-          سلسلات
-        </Text>
-      </Link> */}
-          <NavLink
-            activeStyle={{
-              fontWeight: 'bold',
-              color: 'white',
-              backgroundColor: 'black',
-              height: '100%',
-            }}
-            to="furthercoming?furthercoming=1"
-          >
-            <Flex
-              h="100%"
-              direction="column"
-              justifyContent="center"
-              _hover={{
-                bg: hoverBg[colorMode],
-                color: hoverClr[colorMode],
-              }}
-              _focus={{
-                bg: hoverBg[colorMode],
-                color: hoverClr[colorMode],
-              }}
-              _active={{
-                bg: hoverBg[colorMode],
-                color: hoverClr[colorMode],
-              }}
-            >
-              <Text
-                whiteSpace="nowrap"
-                fontWeight="bold"
-                p="4"
-                fontSize={['lg', '2xl']}
-                fontFamily="diodrum-med !important"
+              {!data && (
+                <Box w="100%" mt="50" mb="50" textAlign="center">
+                  <Spinner size="xl" />
+                </Box>
+              )}
+              <SimpleGrid
+                pl={['5%', '5%', '15%', '15%']}
+                pr={['5%', '5%', '15%', '15%']}
+                py={['5%', '0%', '0%', '0%']}
+                columns={[1, 2, 3, 3, 4]}
               >
-                يأتي قريباً
-              </Text>
-            </Flex>
-          </NavLink>
-        </Flex>
-      )}
-
-      <Drawer placement="right" onClose={onClose} isOpen={isOpen} size="xs">
-        <DrawerOverlay />
-        <DrawerContent
-          bg={colorMode === 'dark' ? bg[colorMode] : '#000'}
-          color="white"
-        >
-          <DrawerHeader p="0">
-            <IconButton
-              p={8}
-              onClick={onClose}
-              icon={<FaTimes size="25px" />}
-              bg="transparent"
-              _hover={{ bg: 'transparent' }}
-              _focus={{ bg: 'transparent' }}
-              _active={{ bg: 'transparent' }}
-            />
-          </DrawerHeader>
-          <DrawerBody p="0" mt="2em">
-            <Flex direction="column">
-              <NavLink
-                activeStyle={{
-                  fontWeight: 'bold',
-                  color: 'white',
-                  backgroundColor: 'black',
-                  height: '100%',
-                  marginLeft: 5,
-                }}
-                to="/featured?featured=1"
-              >
-                <Flex
-                  h="100%"
-                  direction="column"
-                  justifyContent="center"
-                  pt=".5em"
-                  pb="1em"
-                  px="1em"
-                  _hover={{ bg: 'white', color: '#000' }}
-                  _focus={{ bg: 'white', color: '#000' }}
-                  _active={{ bg: 'white', color: '#000' }}
-                >
-                  <Text
-                    whiteSpace="nowrap"
-                    fontWeight="bold"
-                    ml="4"
-                    mr="4"
-                    fontSize={['lg', '2xl']}
-                    fontFamily="diodrum-med !important"
-                  >
-                    آخر الإصدارات
-                  </Text>
-                </Flex>
-              </NavLink>
-              <NavLink
-                activeStyle={{
-                  fontWeight: 'bold',
-                  color: 'white',
-                  backgroundColor: 'black',
-                  height: '100%',
-                  marginLeft: 5,
-                }}
-                to="/books"
-              >
-                <Flex
-                  h="100%"
-                  direction="column"
-                  justifyContent="center"
-                  pt=".5em"
-                  pb="1em"
-                  px="1em"
-                  _hover={{ bg: 'white', color: '#000' }}
-                  _focus={{ bg: 'white', color: '#000' }}
-                  _active={{ bg: 'white', color: '#000' }}
-                >
-                  <Text
-                    whiteSpace="nowrap"
-                    fontWeight="bold"
-                    ml="4"
-                    mr="4"
-                    fontSize={['lg', '2xl']}
-                    fontFamily="diodrum-med !important"
-                  >
-                    جميعها
-                  </Text>
-                </Flex>
-              </NavLink>
-              <Accordion allowToggle>
-                <AccordionItem border="none">
-                  <AccordionButton
-                    pt=".5em"
-                    pb="1em"
-                    px="2em"
-                    _hover={{ bg: 'white', color: '#000' }}
-                    _focus={{ bg: 'white', color: '#000' }}
-                    __active={{ bg: 'transparent' }}
-                  >
-                    <Text
-                      whiteSpace="nowrap"
-                      fontWeight="bold"
-                      fontSize={['lg', '2xl']}
-                      fontFamily="diodrum-med !important"
-                    >
-                      حسب الصنف
-                    </Text>
-                    <AccordionIcon fontSize="25px" />
-                  </AccordionButton>
-                  <AccordionPanel
-                    pb={4}
-                    pt="0"
-                    px="0"
-                    borderRight="1px solid"
-                    borderColor="gray.400"
-                  >
-                    {!data && (
-                      <Box w="100" mt="50" mb="50" textAlign="center">
-                        <Spinner size="xl" />
-                      </Box>
-                    )}
-                    {cat &&
-                      cat.map(category => {
-                        return (
+                {data &&
+                  data.map(serie => {
+                    if (isSmallerThan420) {
+                      return (
+                        <Center key={serie.id}>
                           <Link
                             style={{ margin: '50px !important' }}
                             onClick={handleToggle}
-                            to={`/books_by_category?category=${category.key}`}
-                            key={category.id}
+                            to={`/books_by_serie?serie=${serie.key}`}
                           >
-                            <Box
-                              pt=".4em"
-                              pb=".8em"
-                              px="1em"
-                              _hover={{ bg: 'white', color: '#000' }}
-                              _focus={{ bg: 'white', color: '#000' }}
-                              _active={{ bg: 'white', color: '#000' }}
-                            >
-                              <Box textAlign="right">
-                                <Heading
-                                  dir="rtl"
-                                  fontFamily="diodrum-med !important"
-                                  fontSize={['lg', 'lg', 'xl', 'xl']}
-                                >
-                                  {category.name}
-                                </Heading>
-                              </Box>
+                            <Box m={['0.9', '4']}>
+                              <MenuItem
+                                _focus={{ bg: 'white', color: 'black' }}
+                                _hover={{ bg: 'white', color: 'black' }}
+                                fontSize="xl"
+                              >
+                                <Box display="flex">
+                                  <Heading
+                                    fontFamily="diodrum-med !important"
+                                    fontSize={['lg', 'lg', 'xl', 'xl']}
+                                  >
+                                    {serie.name}
+                                  </Heading>
+                                </Box>
+                              </MenuItem>
                             </Box>
                           </Link>
-                        );
-                      })}
-                  </AccordionPanel>
-                </AccordionItem>
-                <AccordionItem border="none">
-                  <AccordionButton
-                    pt=".5em"
-                    pb="1em"
-                    px="2em"
-                    _hover={{ bg: 'white', color: '#000' }}
-                    _focus={{ bg: 'white', color: '#000' }}
-                    __active={{ bg: 'transparent' }}
-                  >
-                    <Text
-                      whiteSpace="nowrap"
-                      fontWeight="bold"
-                      fontSize={['lg', '2xl']}
-                      fontFamily="diodrum-med !important"
-                    >
-                      سلسلات
-                    </Text>
-                    <AccordionIcon fontSize="25px" />
-                  </AccordionButton>
-                  <AccordionPanel
-                    pb={4}
-                    pt="0"
-                    px="0"
-                    borderRight="1px solid"
-                    borderColor="gray.400"
-                  >
-                    {!data && (
-                      <Box w="100" mt="50" mb="50" textAlign="center">
-                        <Spinner size="xl" />
-                      </Box>
-                    )}
-                    {Array.isArray(data) && data.map(serie => {
-                        return (
-                          <Link
-                            style={{ margin: '50px !important' }}
-                            onClick={handleToggle}
-                            to={`/books_by_series?serie=${serie.name} `}
-                            key={serie.id}
-                          >
-                            <Box
-                              pt=".4em"
-                              pb=".8em"
-                              px="1em"
-                              _hover={{ bg: 'white', color: '#000' }}
-                              _focus={{ bg: 'white', color: '#000' }}
-                              _active={{ bg: 'white', color: '#000' }}
+                        </Center>
+                      );
+                    } else {
+                      return (
+                        <Link
+                          key={serie.id}
+                          style={{ margin: '50px !important' }}
+                          onClick={handleToggle}
+                          to={`/books_by_serie?serie=${serie.key}`}
+                        >
+                          <Box m={['0.9', '4']}>
+                            <MenuItem
+                              _focus={{ bg: 'white', color: 'black' }}
+                              _hover={{ bg: 'white', color: 'black' }}
+                              fontSize="xl"
                             >
-                              <Box textAlign="right">
+                              <Box display="flex">
                                 <Heading
-                                  dir="rtl"
                                   fontFamily="diodrum-med !important"
                                   fontSize={['lg', 'lg', 'xl', 'xl']}
                                 >
                                   {serie.name}
                                 </Heading>
                               </Box>
-                            </Box>
-                          </Link>
-                        );
-                      })}
-                  </AccordionPanel>
-                </AccordionItem>
-              </Accordion>
-            </Flex>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
+                            </MenuItem>
+                          </Box>
+                        </Link>
+                      );
+                    }
+                  })}
+              </SimpleGrid>
+            </MenuList>
+          </Menu>
+        </Flex>
+      )}
     </Flex>
   );
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    getSeries: () => dispatch(getSeries()),
-    getCat: () => dispatch(getCat()),
-  };
-};
+const mapStateToProps = state => ({
+  series: state.series,
+  categories: state.categories,
+});
 
-export default connect(null, mapDispatchToProps)(Navbar);
+export default connect(mapStateToProps, { getSeries, getCat })(Navbar);
